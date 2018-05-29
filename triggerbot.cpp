722bb9fb7143d62598f128683829ab6ee674b2c7
Filipe Marques <filipe.s.o.marques@gmail.com>
2018-05-29 17:06:28 +0100

    Remade program logic

--- triggerbot.cpp
+++ triggerbot.cpp
@@ -1,44 +1,49 @@
 #include <Windows.h>
 #include <iostream>
 #include "Hprocess.h"
-
+/* GLOBAL VARIABLES*/
+//CSGO PROCESS
 CHackProcess fProcess;
-
+//OFFSETS
 DWORD dwLocalPlayer = 0xAA9AB4;
 DWORD dwCrossHairId = 0xB2A4;
 DWORD dwAttack = 0x2EC6AC8;
+DWORD localPlayer;
+
 int id;
+int attack;
 
-struct triggerBot {
-  void ReadMemory() {
-    DWORD localPlayer;
+/* MEMORY FUNCTIONS */
+void ReadMemory() {
 
-    ReadProcessMemory(fProcess.__HandleProcess, 
-        (PBYTE*) (fProcess.__dwordClient + dwLocalPlayer),
-        &localPlayer, sizeof(DWORD),0);
-    ReadProcessMemory(fProcess.__HandleProcess,
-        (PBYTE*) (localPlayer+dwCrossHairId), &id, sizeof(int), 0);
-  
-  }
-};
+  ReadProcessMemory(fProcess.__HandleProcess, 
+      (PBYTE*) (fProcess.__dwordClient + dwLocalPlayer),
+      &localPlayer, sizeof(DWORD),0);
+  ReadProcessMemory(fProcess.__HandleProcess,
+      (PBYTE*) (localPlayer+dwCrossHairId), &id, sizeof(int), 0);
+
+}
+
+void WriteMemory() {
+  int attack = 5;
+  WriteProcessMemory(fProcess.__HandleProcess, 
+      (PBYTE*) (fProcess.__dwordClient + dwAttack), &attack, sizeof(int),0);
+  attack = 4;
+  WriteProcessMemory(fProcess.__HandleProcess, 
+      (PBYTE*) (fProcess.__dwordClient + dwAttack), &attack, sizeof(int),0);
+}
 
 int
 main() {
   fProcess.RunProcess();
-  while(1) {
-    struct triggerBot bot;
-    bot.ReadMemory();
-    std::cout << id << std::endl;
-    Sleep(1);
-    
-    if(id > 0) {
-      int attack = 5;
-      WriteProcessMemory(fProcess.__HandleProcess, 
-          (PBYTE*) (fProcess.__dwordClient + dwAttack), &attack, sizeof(int),0);
-      attack = 4;
-      WriteProcessMemory(fProcess.__HandleProcess, 
-          (PBYTE*) (fProcess.__dwordClient + dwAttack), &attack, sizeof(int),0);
 
+  while(1) {
+    ReadMemory();
+    //Debug
+    std::cout << id << std::endl;
+
+    if(id > 0 && id < 32) {
+      WriteMemory();
     }
   }
 }
