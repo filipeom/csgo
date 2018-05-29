5e5e923c66299c72b9cf22aa2d4998254480a3ec
Filipe Marques <filipe.s.o.marques@gmail.com>
2018-05-29 16:13:38 +0100

    added a triggerbot

---
+++ triggerbot.cpp
@@ -0,0 +1,32 @@
+#include <Windows.h>
+#include <iostream>
+#include "Hprocess.h"
+
+CHackProcess fProcess;
+
+DWORD dwLocalPlayer = 0xAA9AB4;
+DWORD dwCrossHairId = 0xB2A4;
+int id;
+
+struct triggerBot {
+  void ReadMemory() {
+    DWORD localPlayer;
+
+    ReadProcessMemory(fProcess.__HandleProcess, 
+        (PBYTE*) (fProcess.__dwordClient + dwLocalPlayer),
+        &localPlayer, sizeof(DWORD),0);
+    ReadProcessMemory(fProcess.__HandleProcess,
+        (PBYTE*) (localPlayer+dwCrossHairId), &id, sizeof(int), 0);
+  
+  }
+}
+
+int
+main() {
+  while(1) {
+    struct triggerBot bot;
+    bot.ReadMemory();
+    std::cout << id << std::endl;
+    Sleep(1);
+  }
+}
